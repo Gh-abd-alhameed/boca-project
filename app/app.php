347af2ab6b97f02bd4c88572036fc6724f9b6f47
Gh-abd-alhameed <gh.abd.alhameed.97@gmail.com>
2022-11-15 20:44:52 +0200

    fix

--- app/app.php
+++ app/app.php
@@ -8,26 +8,43 @@ return [
 	'locale' => 'en',
 
 	'available_locales' => [
+
 		'en' => [
+
 			"prefix" => "/",
+
 		],
 		'ar' => [
+
 			"prefix" => "/ar"
+
 		],
 	],
 
 	"static_file" => [
-		"assets"
+
+		"assets" =>[
+
+			"prefix"=>"/assets"
+
+		]
+
 	],
 
 	"debug" => true,
 
 	"databases" => [
+
 		"driver" => "mysql",
+
 		"host" => "127.0.0.1",
+
 		"database" => "shop",
+
 		"username" => "root",
+
 		"password" => ""
+
 	]
 
 ];
